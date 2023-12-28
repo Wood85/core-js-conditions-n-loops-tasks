e3b818cd8a20c85c857641f2d5cf377c910f4020
Aleksandr <megalistnn@gmail.com>
2023-12-28 21:59:59 +0300

    feat: implement function isPalindrome

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -189,8 +189,12 @@ function convertNumberToString(numberStr) {
  *  '0123210'   => true
  *  'qweqwe'    => false
  */
-function isPalindrome(/* str */) {
-  throw new Error('Not implemented');
+function isPalindrome(str) {
+  const halfOfLength = Math.floor(str.length / 2);
+  for (let i = 0; i < halfOfLength; i += 1) {
+    if (str[i] !== str[str.length - 1 - i]) return false;
+  }
+  return true;
 }
 
 /**
